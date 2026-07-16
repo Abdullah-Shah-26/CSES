17f5f94ec10df5f9e1064a4070dd1dff2ad310b1
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-07-17 00:22:08 +0530

    Update

--- GRAPH ALGORITHMS/Building_Teams.cpp
+++ GRAPH ALGORITHMS/Building_Teams.cpp
@@ -7,27 +7,29 @@ using vi = vector<int>;
 using vll = vector<long long>;
 static const int MOD = 1e9 + 7;
 
-inline void fastio() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+inline void fastio() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
 
-int n,m;
+int n, m;
 
 //! BFS
-bool bfs(int start, vector<int> &color, vector<vector<int>> &adj){
+bool bfs(int start, vector<int> &color, vector<vector<int>> &adj) {
   queue<int> q;
   q.push(start);
 
   color[start] = 1;
 
-  while(!q.empty()){
+  while (!q.empty()) {
     int u = q.front();
     q.pop();
 
-    for(int v : adj[u]){
-      if(color[v] == 0){
+    for (int v : adj[u]) {
+      if (color[v] == 0) {
         color[v] = 3 - color[u];
         q.push(v);
-      }
-      else if(color[v] == color[u]){
+      } else if (color[v] == color[u]) {
         return false;
       }
     }
@@ -36,25 +38,25 @@ bool bfs(int start, vector<int> &color, vector<vector<int>> &adj){
 }
 
 //! DFS
-bool dfs(int u, vector<int>& color, vector<vector<int>>& adj) {
+bool dfs(int u, vector<int> &color, vector<vector<int>> &adj) {
 
-    for(int v : adj[u]) {
+  for (int v : adj[u]) {
 
-        // not colored yet
-        if(color[v] == 0) {
-            color[v] = 3 - color[u];
+    // not colored yet
+    if (color[v] == 0) {
+      color[v] = 3 - color[u];
 
-            if(!dfs(v, color, adj))
-                return false;
-        }
-
-        // already colored → check conflict
-        else if(color[v] == color[u]) {
-            return false;
-        }
+      if (!dfs(v, color, adj))
+        return false;
     }
 
-    return true;
+    // already colored → check conflict
+    else if (color[v] == color[u]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 int main() {
@@ -63,9 +65,9 @@ int main() {
   cin >> n >> m;
 
   vector<vector<int>> adj(n + 1);
-  
-  for(int i = 0; i < m; i++){
-    int u,v;
+
+  for (int i = 0; i < m; i++) {
+    int u, v;
     cin >> u >> v;
 
     adj[u].push_back(v);
@@ -74,18 +76,18 @@ int main() {
 
   vector<int> color(n + 1, 0); // 0 - not visited, 1 or 2 = team
 
-  for(int i = 1; i <= n; i++){
-    if(color[i] == 0){
-      color[i] = 1; //* Assign color to stNode before dfs 
-      if(!dfs(i, color, adj)){
+  for (int i = 1; i <= n; i++) {
+    if (color[i] == 0) {
+      color[i] = 1; //* Assign color to stNode before dfs
+      if (!dfs(i, color, adj)) {
         cout << "IMPOSSIBLE" << endl;
         return 0;
       }
     }
   }
 
-  for(int i = 1; i <= n; i++){
-    cout << color[i] << " "; 
+  for (int i = 1; i <= n; i++) {
+    cout << color[i] << " ";
   }
   cout << endl;
 
--- GRAPH ALGORITHMS/Message_Route.cpp
+++ GRAPH ALGORITHMS/Message_Route.cpp
@@ -7,24 +7,29 @@ using vi = vector<int>;
 using vll = vector<long long>;
 static const int MOD = 1e9 + 7;
 
-inline void fastio() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+inline void fastio() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
 
-int n,m;
+int n, m;
 
-bool bfs(int st, vector<bool> &vis, vector<int> &parent, vector<vector<int>> &adj){
+bool bfs(int st, vector<bool> &vis, vector<int> &parent,
+         vector<vector<int>> &adj) {
   vis[st] = 1;
-  
+
   queue<int> q;
   q.push(st);
 
-  while(!q.empty()){
+  while (!q.empty()) {
     int u = q.front();
     q.pop();
 
-    if(u == n) return true;
+    if (u == n)
+      return true;
 
-    for(int v : adj[u]){
-      if(!vis[v]){
+    for (int v : adj[u]) {
+      if (!vis[v]) {
         vis[v] = 1;
         parent[v] = u;
         q.push(v);
@@ -42,7 +47,7 @@ int main() {
 
   vector<vector<int>> adj(n + 1);
 
-  for(int i = 0; i < m; i++){
+  for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
     adj[u].push_back(v);
@@ -52,16 +57,16 @@ int main() {
   vector<int> parent(n + 1, -1);
   vector<bool> vis(n + 1, 0);
 
-  if(!bfs(1, vis, parent, adj)){
+  if (!bfs(1, vis, parent, adj)) {
     cout << "IMPOSSIBLE" << endl;
     return 0;
   }
 
   //! Path Reconstruction
   int cur = n;
-  vector<int>path;
+  vector<int> path;
 
-  while(cur != -1){
+  while (cur != -1) {
     path.push_back(cur);
     cur = parent[cur];
   }
@@ -69,8 +74,8 @@ int main() {
   reverse(path.begin(), path.end());
 
   cout << path.size() << endl;
- 
-  for(int i : path){
+
+  for (int i : path) {
     cout << i << " ";
   }
   cout << endl;
